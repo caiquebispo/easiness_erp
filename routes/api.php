c12eaf31750e7d863fef3fb911e449ced23e5d80
Caique Bispo <diogo@guimepa.com.br>
2025-05-19 07:56:25 -0300

    wip

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\{Login, Logout, Register};
+
 Route::prefix('v1/erp')->group(function(){
 
     Route::post('/companies', [Store::class, '__invoke']);
@@ -12,10 +13,8 @@ Route::prefix('v1/erp')->group(function(){
 });
 
 Route::prefix('v1/auth')->group(function(){
-
    Route::post('/login', [Login::class,'__invoke']);
    Route::post('/register', [Register::class,'__invoke']);
    Route::post('/logout', [Logout::class,'__invoke']);
-
 });
 
